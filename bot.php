aeecaadbd9314d9d71faa92925433d3d9d431869
chinubu <jatupol.en@gmail.com>
2017-10-16 06:25:03 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,3 +1,18 @@
 <?php
+$access_token = 'hUs5ogcoeVzkaQvU149w3srkE/eozzGKhpOy29Hx4r0TQxvsyKnpDDdQeSVjem6TqV0dBQ81o1z1iF1NHSPDX7cPvF1C9feQKmeKty6MX1+3iEw81pFuEEecuUEH0+mISEF0UXmiR3skfFlNJCdnNAdB04t89/1O/w1cDnyilFU=';
+$content = file_get_contents('php://input');
+// Parse JSON
 $events = json_decode($content, true);
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
 echo $events;
