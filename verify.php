eed59cd669f238ead5b33cf4d54e9f90a7f99db7
chinubu <jatupol.en@gmail.com>
2017-10-16 06:18:24 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,7 +1,7 @@
 <?php
 $access_token = 'hUs5ogcoeVzkaQvU149w3srkE/eozzGKhpOy29Hx4r0TQxvsyKnpDDdQeSVjem6TqV0dBQ81o1z1iF1NHSPDX7cPvF1C9feQKmeKty6MX1+3iEw81pFuEEecuUEH0+mISEF0UXmiR3skfFlNJCdnNAdB04t89/1O/w1cDnyilFU=';
 
-
+$events = json_decode($content, true);
 $url = 'https://api.line.me/v1/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
@@ -14,3 +14,4 @@ $result = curl_exec($ch);
 curl_close($ch);
 
 echo $result;
+echo $events;
